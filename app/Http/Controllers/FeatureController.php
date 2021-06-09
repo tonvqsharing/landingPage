2aa3b0aab1605bac0c908383fb29e51b09b72333
TonVQ Sharing <43400716+tonvqsharing@users.noreply.github.com>
2021-06-09 10:35:09 +0700

    Add files via upload

---
+++ app/Http/Controllers/FeatureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\feature;
+use Illuminate\Http\Request;
+
+class FeatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\feature  $feature
+     * @return \Illuminate\Http\Response
+     */
+    public function show(feature $feature)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\feature  $feature
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(feature $feature)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\feature  $feature
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, feature $feature)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\feature  $feature
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(feature $feature)
+    {
+        //
+    }
+}
